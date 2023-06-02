1830886fba5e92a6208df4cc3a18870fd959fcc1
Steven Mansfield <stevenmansfield@Stevens-MacBook-Air.local>
2023-06-02 17:55:19 -0600

    Add buttons and arithmetic functionality

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-// I am adding a comment here for additional testing. 
+let num1 = -1; 
+let num2; 
+let operator; 
 
-alert("This is working"); 
+let allNumbers = document.querySelectorAll(".number-button"); 
+[...allNumbers].forEach(function(num) {
+    num.addEventListener("click", () => {
+        numberController(Number(num.textContent)); 
+    })
+});
+
+let allArithmetic = document.querySelectorAll(".arithmetic-button"); 
+[...allArithmetic].forEach(function(arith) {
+    arith.addEventListener("click", () => {
+        operator = arith.getAttribute("id"); 
+    })
+})
+
+function numberController(number) {
+    if (num1 == -1) {
+        num1 = number; 
+    } else {
+        num2 = number; 
+        let result = Number(launchArithmetic(num1, num2, operator)); 
+        console.log(result); 
+    }
+}
+
+function launchArithmetic(number1, number2, operator) {
+    if (operator == "addition") {
+        return number1 + number2; 
+    } else if (operator == "subtraction") {
+        return number1 - number2; 
+    } else if (operator == "multiplication") {
+        return number1 * number2; 
+    } else {
+        return number1 / number2; 
+    }
+}
